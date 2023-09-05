5000593c65d8e22d9252508128d97ffa1fbcf1e5
neeraj <palliyali.neeraj@gmail.com>
2023-09-05 22:49:57 +0100

    changes to config files

--- src/config/mongodb.config.ts
+++
@@ -1,9 +0,0 @@
-import {ConfigProps} from "./config.interface";
-export const config = ():ConfigProps => ({
-  mongodb: {
-    database: {
-      connectionString: process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017',
-      databaseName: process.env.NODE_ENV || 'dev' 
-    }
-  }
- });
